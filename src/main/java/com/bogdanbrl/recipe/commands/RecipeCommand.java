bf933a8fe693921c8b747f9f64a57c8f57a3fd42
Bogdan Brl <bogdan.brlx@gmail.com>
2021-05-20 12:55:45 +0300

    refactoring gradle file entirely

--- src/main/java/com/bogdanbrl/recipe/commands/RecipeCommand.java
+++ src/main/java/com/bogdanbrl/recipe/commands/RecipeCommand.java
@@ -4,16 +4,13 @@ import com.bogdanbrl.recipe.domain.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -39,7 +36,6 @@ public class RecipeCommand {
     private Integer servings;
     private String source;
 
-    @URL
     private String url;
 
     @NotBlank
